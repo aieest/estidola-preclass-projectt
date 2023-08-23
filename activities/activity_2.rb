8bdc7204a6b5a65939d15ecbd1626eec6b542068
Estidola <aie.estidola@gmail.com>
2023-08-23 16:10:00 +0800

    activity 2: created a script that combines all values of a hash into a single value

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,13 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def full_name(name)
+  my_name = name[:first_name] + " " + name[:middle_name] + " " +name[:last_name]
+
+  return my_name
+end
+
+my_name = { first_name: "August", middle_name: "Aresgado", last_name: "Estidola" }
+
+puts full_name(my_name)
