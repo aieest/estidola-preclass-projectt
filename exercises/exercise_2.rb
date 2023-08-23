622965fa460700dadaee7802318dabaaccb313bc
Estidola <aie.estidola@gmail.com>
2023-08-23 14:20:00 +0800

    I tried answering exercise 2

---
+++ exercises/exercise_2.rb
@@ -0,0 +1,12 @@
+# Write a function that takes parameter age and returns 'adult' or 'not adult'. Hint - Legal Age.
+# Print on console the result.
+
+def adultOrNot (age)
+  if age < 18
+    return "Not Adult"
+
+  else
+    return "Adult"
+  end
+
+end
